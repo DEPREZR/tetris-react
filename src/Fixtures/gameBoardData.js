32664432f426c57a30fd748aae4cd28d5449fe1c
Rémi DEPREZ <r.deprez@linkbynet.com>
2019-03-06 00:47:13 +0100

    Add some Fixtures

---
+++ src/Fixtures/gameBoardData.js
@@ -0,0 +1,80 @@
+export const gameBoardDataFixtures = [
+  [
+    [null, null, null, null, null, null, null, null, null, null],
+
+    [null, null, null, null, null, null, null, null, null, null],
+
+    [null, null, null, null, null, null, null, null, null, null],
+
+    [null, null, null, null, null, null, null, null, null, null],
+
+    [null, null, null, null, null, null, null, null, null, null],
+
+    [null, null, null, null, null, null, null, null, null, null],
+
+    [null, null, null, null, null, null, null, null, null, null],
+
+    [null, null, null, null, null, null, null, null, null, null],
+
+    [null, null, null, null, null, null, null, null, null, null],
+
+    [null, null, null, null, null, null, null, null, null, null],
+
+    [null, null, null, null, null, null, null, null, null, null],
+
+    [null, null, null, null, null, null, null, null, null, null],
+
+    [null, null, null, null, null, null, null, null, null, null],
+
+    [null, null, null, null, null, null, null, null, null, null],
+
+    [null, null, null, null, null, null, null, null, null, null],
+
+    [null, null, null, null, null, null, null, null, null, null],
+
+    [null, null, null, null, null, null, null, null, null, null],
+
+    [null, null, null, null, null, null, null, null, null, null],
+
+    [null, null, null, { color: 'blue' }, null, null, null, null, null, null],
+
+    [
+      null,
+      null,
+      null,
+      { color: 'blue' },
+      null,
+      null,
+      null,
+      { color: 'purple' },
+      null,
+      null
+    ],
+
+    [
+      { color: 'red' },
+      null,
+      null,
+      { color: 'blue' },
+      { color: 'yellow' },
+      { color: 'yellow' },
+      { color: 'purple' },
+      { color: 'purple' },
+      null,
+      null
+    ],
+
+    [
+      { color: 'red' },
+      { color: 'red' },
+      { color: 'red' },
+      { color: 'blue' },
+      { color: 'yellow' },
+      { color: 'yellow' },
+      null,
+      { color: 'purple' },
+      null,
+      null
+    ]
+  ]
+];
