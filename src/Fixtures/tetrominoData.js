4ef90728d4d09395645c372faa2929e40ed81d0c
Rémi DEPREZ <r.deprez@linkbynet.com>
2019-03-08 20:36:36 +0100

    Add fixture for tetromino T layer 1

--- src/Fixtures/tetrominoData.js
+++ src/Fixtures/tetrominoData.js
@@ -30,6 +30,36 @@ export const tetrominoDataFixtures = [
     currentLayerIndex: 0
   },
   {
+    position: { x: 3, y: 2 },
+    layers: [
+      [
+        { color: 'purple', x: 1, y: 0 },
+        { color: 'purple', x: 0, y: 1 },
+        { color: 'purple', x: 1, y: 1 },
+        { color: 'purple', x: 2, y: 1 }
+      ],
+      [
+        { color: 'purple', x: 1, y: 0 },
+        { color: 'purple', x: 1, y: 1 },
+        { color: 'purple', x: 2, y: 1 },
+        { color: 'purple', x: 1, y: 2 }
+      ],
+      [
+        { color: 'purple', x: 0, y: 1 },
+        { color: 'purple', x: 1, y: 1 },
+        { color: 'purple', x: 2, y: 1 },
+        { color: 'purple', x: 1, y: 2 }
+      ],
+      [
+        { color: 'purple', x: 1, y: 0 },
+        { color: 'purple', x: 0, y: 1 },
+        { color: 'purple', x: 1, y: 1 },
+        { color: 'purple', x: 1, y: 2 }
+      ]
+    ],
+    currentLayerIndex: 1
+  },
+  {
     position: { x: 3, y: 19 },
     layers: [
       [
