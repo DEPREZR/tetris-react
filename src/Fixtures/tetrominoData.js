30915f4056ec14412e8e71fa28b0807a757eda68
Rémi DEPREZ <r.deprez@linkbynet.com>
2019-03-08 23:07:36 +0100

    Add constant T_TETROMINO_LAYERS and use it in fixtures

--- src/Fixtures/tetrominoData.js
+++ src/Fixtures/tetrominoData.js
@@ -1,92 +1,19 @@
+import { T_TETROMINO_LAYERS } from 'constants.js';
+
 export const tetrominoDataFixtures = [
   {
     position: { x: 3, y: 2 },
-    layers: [
-      [
-        { color: 'purple', x: 1, y: 0 },
-        { color: 'purple', x: 0, y: 1 },
-        { color: 'purple', x: 1, y: 1 },
-        { color: 'purple', x: 2, y: 1 }
-      ],
-      [
-        { color: 'purple', x: 1, y: 0 },
-        { color: 'purple', x: 1, y: 1 },
-        { color: 'purple', x: 2, y: 1 },
-        { color: 'purple', x: 1, y: 2 }
-      ],
-      [
-        { color: 'purple', x: 0, y: 1 },
-        { color: 'purple', x: 1, y: 1 },
-        { color: 'purple', x: 2, y: 1 },
-        { color: 'purple', x: 1, y: 2 }
-      ],
-      [
-        { color: 'purple', x: 1, y: 0 },
-        { color: 'purple', x: 0, y: 1 },
-        { color: 'purple', x: 1, y: 1 },
-        { color: 'purple', x: 1, y: 2 }
-      ]
-    ],
+    layers: T_TETROMINO_LAYERS,
     currentLayerIndex: 0
   },
   {
     position: { x: 3, y: 2 },
-    layers: [
-      [
-        { color: 'purple', x: 1, y: 0 },
-        { color: 'purple', x: 0, y: 1 },
-        { color: 'purple', x: 1, y: 1 },
-        { color: 'purple', x: 2, y: 1 }
-      ],
-      [
-        { color: 'purple', x: 1, y: 0 },
-        { color: 'purple', x: 1, y: 1 },
-        { color: 'purple', x: 2, y: 1 },
-        { color: 'purple', x: 1, y: 2 }
-      ],
-      [
-        { color: 'purple', x: 0, y: 1 },
-        { color: 'purple', x: 1, y: 1 },
-        { color: 'purple', x: 2, y: 1 },
-        { color: 'purple', x: 1, y: 2 }
-      ],
-      [
-        { color: 'purple', x: 1, y: 0 },
-        { color: 'purple', x: 0, y: 1 },
-        { color: 'purple', x: 1, y: 1 },
-        { color: 'purple', x: 1, y: 2 }
-      ]
-    ],
+    layers: T_TETROMINO_LAYERS,
     currentLayerIndex: 1
   },
   {
     position: { x: 3, y: 19 },
-    layers: [
-      [
-        { color: 'purple', x: 1, y: 0 },
-        { color: 'purple', x: 0, y: 1 },
-        { color: 'purple', x: 1, y: 1 },
-        { color: 'purple', x: 2, y: 1 }
-      ],
-      [
-        { color: 'purple', x: 1, y: 0 },
-        { color: 'purple', x: 1, y: 1 },
-        { color: 'purple', x: 2, y: 1 },
-        { color: 'purple', x: 1, y: 2 }
-      ],
-      [
-        { color: 'purple', x: 0, y: 1 },
-        { color: 'purple', x: 1, y: 1 },
-        { color: 'purple', x: 2, y: 1 },
-        { color: 'purple', x: 1, y: 2 }
-      ],
-      [
-        { color: 'purple', x: 1, y: 0 },
-        { color: 'purple', x: 0, y: 1 },
-        { color: 'purple', x: 1, y: 1 },
-        { color: 'purple', x: 1, y: 2 }
-      ]
-    ],
+    layers: T_TETROMINO_LAYERS,
     currentLayerIndex: 0
   }
 ];
--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,28 @@
 export const INTERVAL_BETWEEN_CALLBACKS_TOUCHED_PRESSED = 100;
 export const INTERVAL_AUTO_DOWN = 1000;
+export const T_TETROMINO_LAYERS = [
+  [
+    { color: 'purple', x: 1, y: 0 },
+    { color: 'purple', x: 0, y: 1 },
+    { color: 'purple', x: 1, y: 1 },
+    { color: 'purple', x: 2, y: 1 }
+  ],
+  [
+    { color: 'purple', x: 1, y: 0 },
+    { color: 'purple', x: 1, y: 1 },
+    { color: 'purple', x: 2, y: 1 },
+    { color: 'purple', x: 1, y: 2 }
+  ],
+  [
+    { color: 'purple', x: 0, y: 1 },
+    { color: 'purple', x: 1, y: 1 },
+    { color: 'purple', x: 2, y: 1 },
+    { color: 'purple', x: 1, y: 2 }
+  ],
+  [
+    { color: 'purple', x: 1, y: 0 },
+    { color: 'purple', x: 0, y: 1 },
+    { color: 'purple', x: 1, y: 1 },
+    { color: 'purple', x: 1, y: 2 }
+  ]
+];
